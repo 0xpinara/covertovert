782feb1378422cc59ec91cda5ddb74d65f9e0b2d
YUSUFHAN ALI USTUN <e2522100@ceng.metu.edu.tr>
2024-12-27 09:32:39 +0300

    Implementation of encoding and decoding functions, will need to add documentation.

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -1,6 +1,7 @@
 from CovertChannelBase import CovertChannelBase
 from scapy.layers.l2 import ARP, Ether
 from scapy.all import sniff, get_if_list, conf
+import random
 import time
 
 class MyCovertChannel(CovertChannelBase):
@@ -17,40 +18,94 @@ class MyCovertChannel(CovertChannelBase):
     Channel Capacity: ~16.35 bits/second
     """
     
+    a = 1
+    b = 1
+    c = 1
+    stop_rec = False
+
     def __init__(self):
         super().__init__()
         conf.verb = 0
     
     def encode_bits_to_mac(self, bits):
-        """Helper function to encode 2 bits into a MAC address"""
-        base_mac = "00:11:22:33:"  # First 4 bytes constant
-        # Encode 2 bits using last two bytes
-        if bits == "00":
-            return base_mac + "00:00"
-        elif bits == "01":
-            return base_mac + "00:FF"
-        elif bits == "10":
-            return base_mac + "FF:00"
-        else:  # "11"
-            return base_mac + "FF:FF"
-    
-    def decode_mac_to_bits(self, mac):
-        """Helper function to decode a MAC address into 2 bits"""
-        last_two_bytes = mac.split(":")[-2:]
-        byte5, byte6 = [int(b, 16) for b in last_two_bytes]
+
+        shift = self.c*(self.a + self.b) % 24
+        print(f"Shift: {shift}")
+        self.b = self.a + self.b
+        self.a = self.b - self.a
+        self.c = self.c + 1
+
+        secret_message_shush = []
+        for bit in bits:
+            other_bit = '0' if bit == '1' else '1'
+            truple = [other_bit, other_bit, other_bit]
+            zero_or_two = random.choice([0, 2])
+            if zero_or_two == 2:
+                place = random.choice([0, 1, 2])
+                truple = [bit, bit, bit]
+                truple[place] = other_bit
+            secret_message_shush.extend(truple)
+
+        secret_string = ''.join(secret_message_shush)
+        shifted_secret_string = secret_string[-shift:] + secret_string[:-shift]
+
+        encoded_mac = ""
+        bits48 = []
+        signature = "011000010110100101101110"
         
-        if byte5 == 0 and byte6 == 0:
-            return "00"
-        elif byte5 == 0 and byte6 == 255:
-            return "01"
-        elif byte5 == 255 and byte6 == 0:
-            return "10"
-        else:  # byte5 == 255 and byte6 == 255
-            return "11"
+        shifted_signature = signature[shift:] + signature[:shift]
+        for j in range(24):
+            bits48.append(shifted_signature[j])
+            bits48.append(shifted_secret_string[j])
+        for i in range(0, 48, 8):
+            byte = int("".join(bits48[i:i+8]), 2)
+            encoded_mac += f"{byte:02x}:"
+        print(f"Encoded MAC: {encoded_mac[:-1]}")
+        return encoded_mac[:-1]
+
+    
+    def decode_mac_to_bits(self, bit_v):
+        shift = (self.c-1)*(self.b) % 24
+        decodee = []
+
+        for i in range(24):
+            decodee.append(bit_v[(i*2)+1])
+        
+        garbled_message = ''.join(decodee)
+        shifted_message = garbled_message[shift:] + garbled_message[:shift]
+
+        bits = []
+
+        for j in range(8):
+            ones = 0
+            zeros = 0
+            if shifted_message[j*3] == '1':
+                ones += 1
+            else:
+                zeros += 1
+            if shifted_message[(j*3)+1] == '1':
+                ones += 1
+            else:
+                zeros += 1
+            if shifted_message[(j*3)+2] == '1':
+                ones += 1
+            else:
+                zeros += 1
+            if zeros % 2 == 1:
+                bits.append('1')
+            else:
+                bits.append('0')
+
+        if("".join(bits) == "00101110"):
+            self.stop_rec = True            
+
+        return bits
+
     
     def send(self, min_length, max_length, log_file_name, packet_delay):
         """Send covert message using enhanced ARP packets encoding"""
         message = self.generate_random_message(min_length, max_length)
+        message = "Present day, present time" + "."
         self.log_message(message, log_file_name)
         
         print(f"Sending message: {message}")
@@ -59,15 +114,13 @@ class MyCovertChannel(CovertChannelBase):
         
         start_time = time.time()
         
-        # Send two bits at a time
-        for i in range(0, len(binary_message), 2):
-            bits = binary_message[i:i+2]
-            if len(bits) == 1:  # Handle odd number of bits
-                bits += "0"
+        # Send 8 bits at a time
+        for i in range(0, len(binary_message), 8):
+            bits = binary_message[i:i+8]
                 
             arp = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(
                 op=1,
-                hwsrc="02:42:ac:11:00:02",
+                hwsrc="02:42:ac:11:0a0:02",
                 psrc="172.18.0.2",
                 hwdst=self.encode_bits_to_mac(bits),
                 pdst="172.18.0.3"
@@ -87,15 +140,36 @@ class MyCovertChannel(CovertChannelBase):
         received_bits = []
         print(f"Starting enhanced receiver on eth0 (timeout: {timeout}s)")
         
+        def check_ain(bit_v):
+            shift = self.c*(self.a + self.b) % 24
+            bits = []
+            for i in range(0, len(bit_v), 2):
+                bits.append(bit_v[i])
+            signature = "011000010110100101101110"
+            if "".join(bits) == signature[shift:] + signature[:shift]:
+                self.b = self.a + self.b
+                self.a = self.b - self.a
+                self.c = self.c + 1
+                return True
+            return False
+
         def packet_callback(packet):
             if ARP in packet:
-                # Check if it's our covert channel packet
-                if packet[ARP].hwdst.startswith("00:11:22:33"):
-                    mac = packet[ARP].hwdst
-                    bits = self.decode_mac_to_bits(mac)
-                    received_bits.extend(list(bits))
-                    print(f"Received bits: {bits} from MAC: {mac}")
-        
+                mac = packet[ARP].hwdst
+                mac = mac.replace(":", "")
+                bit_v = []
+                for he in mac:
+                    bin = format(int(he, 16), '04b')
+                    bit_v.append(bin[0])
+                    bit_v.append(bin[1])
+                    bit_v.append(bin[2])
+                    bit_v.append(bin[3])
+                if check_ain(bit_v):                    
+                    bits = self.decode_mac_to_bits(bit_v)
+                    received_bits.extend(bits)
+                    print(f"Received bits: {bits} from MAC: {packet[ARP].hwdst}")
+
+
         try:
             print("Waiting for packets...")
             sniff(
@@ -103,7 +177,8 @@ class MyCovertChannel(CovertChannelBase):
                 filter="arp",
                 prn=packet_callback,
                 timeout=timeout,
-                store=0
+                store=0,
+                stop_filter=lambda p: self.stop_rec
             )
         except Exception as e:
             print(f"Error during capture: {e}")
